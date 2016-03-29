461e92317833eb64962c3c564dd5f0ebb0991ef0
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-03-29 17:39:00 +0900

    first commit

---
+++ ColorValuesChangedBugRepro/js/default.js
@@ -0,0 +1,18 @@
+﻿// Minimal template without WinJS
+Windows.UI.WebUI.WebUIApplication.addEventListener("activated", ev => {
+    if (ev.detail[0].kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
+        initializeAccentColorReceiver();
+    }
+})
+
+const uiSettings = new Windows.UI.ViewManagement.UISettings();
+
+function initializeAccentColorReceiver() {
+    showCurrentAccentColor();
+    uiSettings.addEventListener("colorvalueschanged", showCurrentAccentColor);
+}
+
+function showCurrentAccentColor() {
+    const color = uiSettings.getColorValue(Windows.UI.ViewManagement.UIColorType.accent)
+    accentColorBox.style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+}
